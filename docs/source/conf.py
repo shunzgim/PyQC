7d107e53e158e9952414e551dcd0236df4310fb2
shunzgim <shunzGIM@163.com>
2021-09-04 21:24:15 +0800

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,11 +15,10 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
-
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
     
-# html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Project information -----------------------------------------------------
 
